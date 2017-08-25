acca3617b18ab9103417d76a51a62d940a04499b
itsouvalas <itsouvalas@engineyard.com>
2017-08-25 13:10:36 +0000

    killed connections

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,4 +1,4 @@
 on_app_master do
-  run 'echo "SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname=\'database_name\';" | psql -U postgres'
+  run 'echo "SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname=\'spina_sample_app\';" | psql -U postgres'
   run "bundle exec rake db:load_sample_if_empty_db DISABLE_DATABASE_ENVIRONMENT_CHECK=1"
 end
